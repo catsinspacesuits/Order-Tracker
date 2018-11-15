a86e5dda8d81cb1103d22525350384bd553a02f4
Scott Munro <simunro@hotmail.co.uk>
2018-11-15 13:57:27 +0100

    added JS for geolocation and button to index view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,17 @@
 //= require jquery
 //= require bootstrap-sprockets
 
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+}
